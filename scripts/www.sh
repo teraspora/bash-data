f4ab4a0193ee0bdb4ed7e9905ca7bf747fc53c89
John Lynch <johnlynch.32768@gmail.com>
2024-07-31 16:59:52 +0100

    Update www.sh to obviate dependency on a preexisting template file

--- scripts/www.sh
+++ scripts/www.sh
@@ -1,20 +1,48 @@
 #!/bin/bash
 
-# in /usr/local/bin
-# www.sh
-# Should be aliased to `www`.
+# I am www.sh and I like to live in /usr/local/bin!
+# It's a good idea to alias me as `www`.
+# Don't forget to make me executable with `chmod`.
 # Usage: `www <dir-to-create> <title> <description>`
 
 # Make a directory inside the current directory, (first arg should be the name of this),
-# copy frontend template file structure to it, then change to it.
-# Fill in title and description from arguments supplied
+# then change to it.
+# Create index.html file, CSS file and JS file in appropriately named subdirectories.
+# Fill in the title and description in the HTML file from the second and third arguments supplied, respectively.
 
 mkdir $1
 cd $1
-cp -r /media/john/sys2/web18/playground/templates/web_fe/* .
-sed -i "s/Some web project/$2/" index.html
-sed -i "s/dummy content/$3/" index.html
-subl .
+cat <<EOF > index.html 
+<!DOCTYPE html>
+<html>
 
+<head>
+    <meta charset="utf-8">
+    <title>Project name goes here</title>
+    <meta name="description" content="Description goes here">
+    <meta name="author" content="John Lynch">
+    <link rel="stylesheet" type="text/css" href="styles/main.css">
+</head>
 
+<body>
+    <canvas id="root-canvas">
+    </canvas>
+
+/* HTML content */
+	
+<script type="text/javascript" src="scripts/main.js"></script>
+
+</body>
+
+</html>
+
+EOF
+
+sed -i "s/Project name goes here/$2/" index.html
+sed -i "s/Description goes here/$3/" index.html
+mkdir styles
+mkdir scripts
+touch styles/main.css
+touch scripts/main.js
+echo -e "\n# Project $2 created at $PWD\n ==============================\n"
 
